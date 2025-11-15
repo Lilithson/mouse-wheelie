8b4e77a95d18f546e66d6c317bb30cbe848398df
Siphalor <info@siphalor.de>
2025-11-15 02:51:00 +0100

    fix: Fix missing import

--- src/main/java/de/siphalor/mousewheelie/client/MWClient.java
+++ src/main/java/de/siphalor/mousewheelie/client/MWClient.java
@@ -45,6 +45,7 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.component.DataComponents;
+//- import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
